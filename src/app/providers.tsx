b653b9b406c8be01abb7fdfa59e4bf9b2da3876a
johngerome <johngerome@gmail.com>
2023-10-09 19:32:13 +0800

    types: missing children type

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-export default function Providers({ children }) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
