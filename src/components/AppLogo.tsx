c2cfdf42b6b01de54af1e89eefa335374ec5f774
johngerome <johngerome@gmail.com>
2023-10-29 18:39:04 +0800

    fix: make logo clickable

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { Icon } from '@iconify/react';
+import Link from 'next/link';
 
 export default function AppLogo() {
   return (
-    <h2 className='flex items-center space-x-3 font-bold'>
-      <Icon icon={'fluent:poll-20-filled'} width={24} />{' '}
-      <span>Cabantian Election 2023</span>
-    </h2>
+    <Link href={'/'}>
+      <h2 className='flex items-center space-x-3 font-bold'>
+        <Icon icon={'fluent:poll-20-filled'} width={24} />{' '}
+        <span>Cabantian Election 2023</span>
+      </h2>
+    </Link>
   );
 }
